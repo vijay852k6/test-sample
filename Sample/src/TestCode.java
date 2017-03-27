92d704c8efeb8202357d4b8e77d1757ef077cc25
vsgayu <vsgayu@LAPTOP-TJGMBVFH.attlocal.net>
2017-03-26 20:05:25 -0500

    Intial commit test code

---
+++ Sample/src/TestCode.java
@@ -0,0 +1,14 @@
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		doSomething();
+
+	}
+
+	public static void doSomething()
+	{
+		System.out.println("Test code");
+	}
+}
